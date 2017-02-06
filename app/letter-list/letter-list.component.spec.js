a1e46ec302c54237f2d99f14995634cf0ce2eced
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-02-06 10:24:16 -0600

    added unit tests, working on fixing reference error for karma test

--- app/letter-list/letter-list.component.spec.js
+++ app/letter-list/letter-list.component.spec.js
@@ -15,17 +15,17 @@ describe('letterList', function() {
     beforeEach(inject(function($componentController, _$httpBackend_) {
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('letters/letters.json')
-                  .respond([{id: 'Ep3-7'}]);
+                  .respond([{id: 'ep3-7'}]);
       ctrl = $componentController(letterList);
     }));
 
-    it('should create a `letters` property with 2 letters fetched with `$http`', function() {
+    it('should create a `letters` property with 1 letter fetched with `$http`', function() {
       jasmine.addCustomEqualityTester(angular.equals);
 
       expect(ctrl.letters).toEqual([]);
 
       $httpBackend.flush();
-      expect(ctrl.phones).toEqual([{id: 'Ep3-7'}]);
+      expect(ctrl.phones).toEqual([{id: 'ep3-7'}]);
     });
   })
 });
---
+++ e2e-test/protractor.conf.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Tyler on 2/6/17.
+ */
+//jshint strict: false
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+
+};
